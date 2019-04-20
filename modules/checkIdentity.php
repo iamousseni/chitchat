d65c769b48e80cdee4a5bfa0a1133e8523f90e00
iamousseni <ousseni.work@gmail.com>
2019-04-20 14:01:42 +0200

    controllo identità utente senza eventi db

--- modules/checkIdentity.php
+++ modules/checkIdentity.php
@@ -3,13 +3,27 @@ include 'Admin/include/config.php';
 if(isset($_GET['u']) && isset($_GET['ds'])){
     $username = addslashes($_GET['u']);
     $dataRichiesta = addslashes($_GET['ds']);
-    $dataRichiesta = date("Y-m-d H:i:s",$dataRichiesta);
+/*
+  -->  eventualità che gli eventi non siano possibili decommentare questa parte <--
 
-    $result = $mysqli->query("UPDATE utente SET active = 1 WHERE username = '$username' AND dataOraCreazione = '$dataRichiesta';");
-    if(!$result){
+    $dataRichiesta = new DateTime(date("Y-m-d H:i:s", $dataRichiesta));
+    $now = new DateTime(date('Y-m-d H:i:s'));
+    $diff = $dataRichiesta->diff($now)->format('%a');
+
+    if($diff > 0){
+    */
+        $result = $mysqli->query("UPDATE utente SET active = 1 WHERE username = '$username' AND dataOraCreazione = '$dataRichiesta';");
+        if(!$result){
+            $_SESSION['message'] = 'Errore! Questa richiesta di attivazione è scaduta';
+            header('location: ./');
+        }else{
+            header('location: completeProfile');
+        }
+   /* }else{
         $_SESSION['message'] = 'Errore! Questa richiesta di attivazione è scaduta';
         header('location: ./');
-    }else{
-        header('location: completeProfile');
-    }
+    } */
+}else{
+    $_SESSION['message'] = 'Errore! Parametri non passati';
+    header('location: ./');
 }

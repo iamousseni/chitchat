d9cf9800cec5e78918e963499270cc60e7978144
iamousseni <ousseni.work@gmail.com>
2019-04-30 12:46:58 +0200

    maked slideChat ajax

--- modules/API/slideChatAPI.php
+++ modules/API/slideChatAPI.php
@@ -1,8 +1,9 @@
 <?php
     include  '../../Admin/include/config.php';
-    $username = $_COOKIE['u'];
+    $username = $_GET['u'];
+    $limit = $_GET['limit'];
     $slideChat = $mysqli->query("SELECT userHeader.nome, userHeader.cognome, userHeader.pathImageProfile, user2.codUtente, user1.codChat, h.codUtente AS lastUserSender, h.testo, h.pathFile, h.maggiore AS dataOraInvio FROM appartenere AS user1 INNER JOIN appartenere AS user2 ON user1.codChat = user2.codChat INNER JOIN utente AS userHeader ON userHeader.username = user2.codUtente INNER JOIN (SELECT * FROM messaggio INNER JOIN 
-    (SELECT MAX(dataOraInvio) AS maggiore FROM `messaggio` GROUP BY codChat ORDER BY maggiore DESC) as header ON messaggio.dataOraInvio = header.maggiore GROUP BY messaggio.codChat) AS h ON user1.codChat = h.codChat WHERE user1.codUtente = 'iamousseni' GROUP BY user1.codChat;");
+    (SELECT MAX(dataOraInvio) AS maggiore FROM `messaggio` GROUP BY codChat ORDER BY maggiore DESC) as header ON messaggio.dataOraInvio = header.maggiore GROUP BY messaggio.codChat) AS h ON user1.codChat = h.codChat WHERE user1.codUtente = 'iamousseni' GROUP BY user1.codChat LIMIT $limit;");
 
     for($chats = []; $row = $slideChat->fetch_assoc();){
         $chats[] = $row;
--- modules/slideChat.php
+++ modules/slideChat.php
@@ -1,3 +1,4 @@
+<?php include 'Admin/include/config.php'; ?>
 <div>
     <div>
         <h1>Chat</h1>
@@ -10,8 +11,50 @@
     </div>
 </div>
 
+<div id="demo"></div>
 <script>
+
+function createChat(chat){
     
+    let date = new Date(chat['dataOraInvio']);
+    const month = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+    let message = chat['testo'] == null ? "<img src='"+chat['pathFile']+"'>" : chat['testo']; 
+    let result = `
+        <div>
+            <div>
+                <img src="" alt="`+chat['codUtente']+`">
+            </div>
+            <div>
+                <div>
+                    <strong><span>`+chat['nome']+` `+chat['cognome']+`</span></strong>
+                    <span>`+date.getDate()+` `+month[date.getMonth()]+`</span>
+                </div>
+                <div>
+                    <span>`+message+`</span>
+                </div>
+            </div>
+        </div>`
+    ;
+    return result;
+}
 
-
+setInterval(() => {
+    var chatsData;
+    let body = '';
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                if(this.responseText!= undefined){
+                    let chats = JSON.parse(this.responseText);
+                    
+                    for(x=0; x < chats.length; x++){
+                        body += createChat(chats[x]);
+                    }
+                }
+                document.getElementById('demo').innerHTML = body;
+            }
+        };
+        xhttp.open("GET", "API/slideChatAPI.php?u="+document.cookie.u+'&limit=10', true);
+        xhttp.send();
+}, 10000);
 </script>

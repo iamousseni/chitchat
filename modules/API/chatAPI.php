4d54d4ad2b12361b7f0d0c0fd4a5c06d5d0c4338
iamousseni <ousseni.work@gmail.com>
2019-05-04 10:25:47 +0200

    slideChat.php api non attendibile

--- modules/API/chatAPI.php
+++ modules/API/chatAPI.php
@@ -2,14 +2,13 @@
     include  '../../Admin/include/config.php';
     $username = $_GET['u'];
     $limit = $_GET['limit'];
+    $idChat = $_GET['idChat'];
     $slideChat = $mysqli->query("SELECT (@cnt:= @cnt +1) AS id, userHeader.nome, userHeader.cognome, userHeader.pathImageProfile, user2.codUtente, user1.codChat, h.codUtente AS lastUserSender, h.testo, h.pathFile, h.maggiore AS dataOraInvio FROM appartenere AS user1 CROSS JOIN(SELECT @cnt:=0) AS idCounter INNER JOIN appartenere AS user2 ON user1.codChat = user2.codChat INNER JOIN utente AS userHeader ON userHeader.username = user2.codUtente INNER JOIN (SELECT * FROM messaggio INNER JOIN 
-    (SELECT MAX(dataOraInvio) AS maggiore FROM `messaggio` GROUP BY codChat ORDER BY maggiore DESC) as header ON messaggio.dataOraInvio = header.maggiore GROUP BY messaggio.codChat) AS h ON user1.codChat = h.codChat WHERE user1.codUtente = '$username' AND user2.codUtente != '$username' GROUP BY user1.codChat LIMIT $limit;"); 
-    
+    (SELECT MAX(dataOraInvio) AS maggiore FROM `messaggio` WHERE codChat='$idChat' GROUP BY codChat ORDER BY maggiore DESC) as header ON messaggio.dataOraInvio = header.maggiore GROUP BY messaggio.codChat) AS h ON user1.codChat = h.codChat WHERE user1.codUtente = '$username' GROUP BY user1.codChat LIMIT $limit;");
 
     for($chats = []; $row = $slideChat->fetch_assoc();){
         $chats[] = $row;
     }
-
 
     header('Content-Type: application/json');
     echo json_encode($chats);
--- modules/slideChat.php
+++ modules/slideChat.php
@@ -11,9 +11,55 @@
     </div>
 </div>
 
-<div id="demo"></div>
+<div id="chats">
+<?php
+    $json = file_get_contents('http://localhost/chitchat/API/slideChatAPI.php?u=iamousseni&limit=10');
+    $objs = json_decode($json);
+    
+    $month = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+    $result = '';
+    
+    foreach($objs as $obj){
+        
+        $message = $obj->testo == null ? "<img src='".$obj->pathFile."'>" : $obj->testo;
+        $result .= '
+        <div>
+            <div>
+                <img src="'.$obj->pathImageProfile.'" alt="'.$obj->codUtente.'">
+            </div>
+            <div>
+                <div>
+                    <strong><span>'.$obj->nome.' '.$obj->cognome.'</span></strong>
+                    <span>'.date('d', strtotime($obj->dataOraInvio)).' '.$month[date('n', strtotime($obj->dataOraInvio))].'</span>
+                </div>
+                <div>
+                    <span>'.$message.'</span>
+                </div>
+            </div>
+        </div>'
+    ;
+    }
+
+    echo $result;
+?>
+</div>
 <script>
 
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return false;
+}
+    
 function createChat(chat){
     
     let date = new Date(chat['dataOraInvio']);
@@ -22,7 +68,7 @@ function createChat(chat){
     let result = `
         <div>
             <div>
-                <img src="" alt="`+chat['codUtente']+`">
+                <img src="`+chat['pathImageProfile']+`" alt="`+chat['codUtente']+`">
             </div>
             <div>
                 <div>
@@ -35,26 +81,53 @@ function createChat(chat){
             </div>
         </div>`
     ;
-    return result;
-}
 
-setInterval(() => {
-    var chatsData;
-    let body = '';
+    setInterval(() => {
+    let chats;
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 if(this.responseText!= undefined){
-                    let chats = JSON.parse(this.responseText);
+                     chats = JSON.parse(this.responseText);
+                     
+                     if(chats[0]['dataOraInvio'] != localStorage.getItem('chat'+chat['id'])){
+                        localStorage.setItem('chat'+chat['id'], chats[0]['dataOraInvio']);
+                    }
+                    
+                }
+            }
+        };
+        xhttp.open("GET", "API/chatAPI.php?u="+getCookie('u')+'&idChat='+chat['id']+'&limit=10', true);
+        xhttp.send();
+    }, 1000);
+
+    return result;
+}
+
+
+setInterval(() => {
+    let body = '';
+    let chats;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                if(this.responseText!= undefined){
+                     chats = JSON.parse(this.responseText);
                     
                     for(x=0; x < chats.length; x++){
                         body += createChat(chats[x]);
                     }
                 }
-                document.getElementById('demo').innerHTML = body;
+
+                if(chats[chats.length - 1]['id'] != localStorage.getItem('lastChat')){
+                    document.getElementById('chats').innerHTML = body;
+                    localStorage.setItem('lastChat', chats[chats.length -1]['id']);
+                    console.log('nuova chat');
+                }
             }
         };
-        xhttp.open("GET", "API/slideChatAPI.php?u="+document.cookie.u+'&limit=10', true);
+        xhttp.open("GET", "API/slideChatAPI.php?u="+getCookie('u')+'&limit=10', true);
         xhttp.send();
-}, 10000);
-</script>
+}, 1000);
+
+</script> 

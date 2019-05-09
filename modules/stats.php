09a5fdacefd492ae2f976d541ba825df68f15b63
ElCapitanGoldrex <siliquariccardo@gmail.com>
2019-05-09 09:33:06 +0200

    inserimento pagine statistiche

---
+++ modules/stats.php
@@ -0,0 +1,54 @@
+
+<div class="col-12 personInfo">
+	<div class="txt-center">
+	<h1>Chat Statistics</h1>
+	</div>
+	<div class="pt-5">
+		
+		<div class="col-12 left">
+			<div class="col-12 left">
+			<div class="col-6 left">
+				<svg id="i-mail" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="green" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+    				<path d="M2 26 L30 26 30 6 2 6 Z M2 6 L16 16 30 6" />
+				</svg>
+			Messages Sent:
+			</div>
+			<div class="col-6 left">100</div>
+			</div>
+		</div>
+		<div class="col-12 left">
+			<div class="col-12 left">
+			<div class="col-6 left">
+				<svg id="i-mail" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+    				<path d="M2 26 L30 26 30 6 2 6 Z M2 6 L16 16 30 6" />
+				</svg>
+			Messages Recieved:
+			</div>
+			<div class="col-6 left">100</div>
+			</div>
+		</div>
+		<div class="col-12 left">
+			<div class="col-12 left">
+			<div class="col-6 left">
+				<svg id="i-photo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="green" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+    				<path d="M20 24 L12 16 2 26 2 2 30 2 30 24 M16 20 L22 14 30 22 30 30 2 30 2 24" />
+   				 	<circle cx="10" cy="9" r="3" />
+				</svg>
+			Media Sent:
+			</div>
+			<div class="col-6 left">100</div>
+			</div>
+		</div>
+		<div class="col-12 left">
+			<div class="col-12 left">
+			<div class="col-6 left">
+				<svg id="i-photo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+    				<path d="M20 24 L12 16 2 26 2 2 30 2 30 24 M16 20 L22 14 30 22 30 30 2 30 2 24" />
+   				 	<circle cx="10" cy="9" r="3" />
+				</svg>
+			Media Recieved: </div>
+			<div class="col-6 left">100</div>
+			</div>
+		</div>
+	</div>
+</div>

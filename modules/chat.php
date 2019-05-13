acfd32a978a74e3a6a1722eb09dd2da4fcb87784
Ivan Ravasi <ivan4.ravasi@gmail.com>
2019-05-13 13:43:04 +0200

    Sposta <script> in .JS esterno :recycle:

--- modules/chat.php
+++ modules/chat.php
@@ -18,26 +18,4 @@
     </div>
 </div>
 <script src="js/chatAjax.js"></script>
-<script>
-document.getElementById('input').children[0].addEventListener('keypress', function(e){
-    var content = htmlspecialchars(this.value);
-    var idChat = localStorage.getItem('chatOpen');
-    if(e.keyCode === 13){
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                if (this.responseText != undefined) {
-                    document.getElementById('input').children[0].innerHTML = '';
-                    document.getElementById('input').children[0].style.color = '#000';
-                }
-            }
-        };
-        xhttp.open("GET", "elaborator/sendMessage&idChat="+idChat+"&m="+content, true);
-        xhttp.send();
-    }
-});
-
-
-
-
-</script>
+<script src="js/sendMessage.js"></script>

c4bbfd430972bd602cc1b7399acec1d5e41ec0c6
Ivan Ravasi <ivan4.ravasi@gmail.com>
2019-05-23 08:29:52 +0200

    Modifica la query per case-insensitive search
    La query è stata anche indentata per maggiore leggibilità

--- modules/API/searchChatAPI.php
+++ modules/API/searchChatAPI.php
@@ -2,8 +2,26 @@
     include  '../../Admin/include/config.php';
     $username = $_GET['u'];
     $search = $_GET['search'];
-   
-    $searchQuery = $mysqli->query("SELECT * FROM (SELECT (@cnt:= @cnt +1) AS id, us2.nome, us2.cognome, us2.pathImageProfile, user2.codUtente, user2.codChat, h.nome AS senderNome, h.cognome AS senderCognome, h.codUtente AS lastUserSender, h.testo, h.pathFile, h.dataOraInvio FROM appartenere AS user1 CROSS JOIN(SELECT @cnt:=0) AS idCounter INNER JOIN appartenere AS user2 ON user1.codChat = user2.codChat INNER JOIN utente AS us2 ON us2.username = user2.codUtente  INNER JOIN (SELECT * FROM messaggio INNER JOIN utente ON messaggio.codUtente = utente.username ) AS h ON user1.codChat = h.codChat WHERE user1.codUtente = '$username' AND user2.codUtente != '$username' ORDER BY dataOraInvio DESC) AS messaggi WHERE messaggi.nome LIKE '%$search%' OR messaggi.cognome LIKE '%$search%' OR messaggi.codUtente LIKE '%$search%' OR messaggi.testo LIKE '%$search%' LIMIT 3;");
+
+    $searchQuery = $mysqli->query("SELECT *
+      FROM (
+        SELECT (@cnt:= @cnt +1) AS id, us2.nome, us2.cognome, us2.pathImageProfile, user2.codUtente, user2.codChat,
+          h.nome AS senderNome, h.cognome AS senderCognome, h.codUtente AS lastUserSender, h.testo, h.pathFile, h.dataOraInvio
+        FROM appartenere AS user1
+          CROSS JOIN (SELECT @cnt:=0) AS idCounter
+          INNER JOIN appartenere AS user2 ON user1.codChat = user2.codChat
+          INNER JOIN utente AS us2 ON us2.username = user2.codUtente
+          INNER JOIN (
+            SELECT *
+            FROM messaggio INNER JOIN utente ON messaggio.codUtente = utente.username
+          ) AS h ON user1.codChat = h.codChat
+        WHERE user1.codUtente = '$username' AND user2.codUtente != '$username'
+        ORDER BY dataOraInvio DESC
+      ) AS messaggi
+      WHERE LOWER(messaggi.nome) LIKE LOWER('%$search%') OR
+            LOWER(messaggi.cognome) LIKE LOWER('%$search%') OR
+            LOWER(messaggi.codUtente) LIKE LOWER('%$search%') OR
+            LOWER(messaggi.testo) LIKE LOWER('%$search%') LIMIT 3;");
 
     for($resultSearch = []; $row = $searchQuery->fetch_assoc();){
         $resultSearch[] = $row;
